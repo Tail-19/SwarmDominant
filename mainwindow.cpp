d59b7250bfcbf7cb7a1666c4309652c59b35fcdc
Tail-19 <zhenghanlu2020@163.com>
2024-09-05 19:57:48 +0800

    finish all the terminals, ready to release

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,21 +38,29 @@ MainWindow::MainWindow(QWidget *parent)
     connect(delta_process, SIGNAL(readyReadStandardOutput()), this, SLOT(delta_readBashStandardOutput()));
     connect(delta_process, SIGNAL(readyReadStandardError()), this, SLOT(delta_readBashStandardError()));
 
-    // epsilon_process = new QProcess;
-    // epsilon_process->start("bash");
-    // epsilon_process->waitForStarted();
-    // connect(epsilon_process, SIGNAL(readyReadStandardOutput()), this, SLOT(epsilon_readBashStandardOutput()));
-    // connect(epsilon_process, SIGNAL(readyReadStandardError()), this, SLOT(epsilon_readBashStandardError()));
+    epsilon_process = new QProcess;
+    epsilon_process->start("bash");
+    epsilon_process->waitForStarted();
+    connect(epsilon_process, SIGNAL(readyReadStandardOutput()), this, SLOT(epsilon_readBashStandardOutput()));
+    connect(epsilon_process, SIGNAL(readyReadStandardError()), this, SLOT(epsilon_readBashStandardError()));
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+    delete alpha_process;
     delete beta_process;
     delete gamma_process;
+    delete delta_process;
+    delete epsilon_process;
 }
 
 void MainWindow::on_lineEdit_returnPressed() {
     QString _str = ui->lineEdit->text();
+
+    if (ui->alpha_checkBox->isChecked()) {
+        ui->alphaTerminal->append("alpha@alpha-sr:~$ " + _str);
+        alpha_process->write(_str.toLocal8Bit() + '\n');
+    }
 
     if (ui->beta_checkBox->isChecked()) {
         ui->betaTerminal->append("beta@beta-sr:~$ " + _str);
@@ -64,12 +72,28 @@ void MainWindow::on_lineEdit_returnPressed() {
         gamma_process->write(_str.toLocal8Bit() + '\n');
     }
 
+    if (ui->delta_checkBox->isChecked()) {
+        ui->deltaTerminal->append("delta@delta-sr:~$ " + _str);
+        delta_process->write(_str.toLocal8Bit() + '\n');
+    }
+
+    if (ui->epsilon_checkBox->isChecked()) {
+        ui->epsilonTerminal->append("epsilon@epsilon-sr:~$ " + _str);
+        epsilon_process->write(_str.toLocal8Bit() + '\n');
+    }
+
     ui->lineEdit->clear();
 }
 
 
 void MainWindow::on_connectButton_clicked() {
     ui->connectButton->setText("Connecting");
+
+    if (ui->alpha_checkBox->isChecked()) {
+        QString _alpha_ssh = "ssh -tt alpha@alpha-sr";
+        ui->alphaTerminal->append("alpha@alpha-sr:~$ " + _alpha_ssh);
+        alpha_process->write(_alpha_ssh.toLocal8Bit() + '\n');
+    }
 
     if (ui->beta_checkBox->isChecked()) {
         QString _beta_ssh = "ssh -tt beta@beta-sr";
@@ -81,6 +105,18 @@ void MainWindow::on_connectButton_clicked() {
         QString _gamma_ssh = "ssh -tt gamma@gamma-sr";
         ui->gammaTerminal->append("gamma@gamma-sr:~$ " + _gamma_ssh);
         gamma_process->write(_gamma_ssh.toLocal8Bit() + '\n');
+    }
+
+    if (ui->delta_checkBox->isChecked()) {
+        QString _delta_ssh = "ssh -tt delta@delta-sr";
+        ui->deltaTerminal->append("delta@delta-sr:~$ " + _delta_ssh);
+        delta_process->write(_delta_ssh.toLocal8Bit() + '\n');
+    }
+
+    if (ui->epsilon_checkBox->isChecked()) {
+        QString _epsilon_ssh = "ssh -tt epsilon@epsilon-sr";
+        ui->deltaTerminal->append("epsilon@epsilon-sr:~$ " + _epsilon_ssh);
+        epsilon_process->write(_epsilon_ssh.toLocal8Bit() + '\n');
     }
 
     ui->connectButton->setText("Connect");
